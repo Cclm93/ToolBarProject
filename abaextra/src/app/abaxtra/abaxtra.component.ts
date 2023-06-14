7d6fda3c2591eba46fe6d8e737ae9e2213e4882c
Caio Monte <ccldm200@gmail.com>
2023-06-14 10:28:48 -0300

    Update

---
+++ abaextra/src/app/abaxtra/abaxtra.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abaxtra',
+  templateUrl: './abaxtra.component.html',
+  styleUrls: ['./abaxtra.component.scss']
+})
+export class AbaxtraComponent {
+
+}
