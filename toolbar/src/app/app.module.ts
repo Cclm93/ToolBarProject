a4fd228772edb0d34f76802019be46040d7309f3
Caio Monte <ccldm200@gmail.com>
2023-06-20 10:51:07 -0300

    Continuacao

--- toolbar/src/app/app.module.ts
+++ toolbar/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BarComponent } from './bar/bar.component';
 import { Aba2Component } from './aba2/aba2.component';
 import { Aba1Component } from './aba1/aba1.component';
 import { ModulodefuncionalidadeModule } from './modulodefuncionalidade/modulodefuncionalidade.module';
-import { AppComponent2 }from 'C:\Users\ccldm\Documents\GitHub\ToolBarProject\abaextra\src\app\app.module.ts';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { AppComponent2 }from 'C:\Users\ccldm\Documents\GitHub\ToolBarProject\aba
     AppRoutingModule,
      FormsModule,
      ModulodefuncionalidadeModule,
-     AppComponent2,
+  
      
     
      
